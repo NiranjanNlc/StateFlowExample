994d0548f718e1f3aafe72a3b53e3e589a9f5240
niranjannlc <niranjannlc10@gmail.com>
2022-08-03 12:35:49 +0545

    implementation the consumer and producer

--- app/src/main/java/org/lniranjan/stateflowexample/MainActivity.kt
+++ app/src/main/java/org/lniranjan/stateflowexample/MainActivity.kt
@@ -3,6 +3,14 @@ package org.lniranjan.stateflowexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+
 
 class MainActivity : AppCompatActivity() {
     val TAG = "Mainactivity"
@@ -10,6 +18,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.i(TAG," CHECKING BRO ")
-
+       val flow1 = flow<Int> {
+           for (i in 1.. 10)
+           {
+               emit(i)
+               delay(1000)
+           }
+       }
+        lifecycleScope.launch {
+            flow1.buffer().
+            filter { it%2==0 }.
+            map { it *it }.collect{
+                Log.d("main",it.toString())
+            }
+        }
     }
 }
