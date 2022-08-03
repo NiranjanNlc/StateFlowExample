0b2f537ae9815a1373731d8fa56e188a400773e9
niranjannlc <niranjannlc10@gmail.com>
2022-08-03 11:51:12 +0545

    adding dependancy required

--- app/src/main/java/org/lniranjan/stateflowexample/MainActivity.kt
+++ app/src/main/java/org/lniranjan/stateflowexample/MainActivity.kt
@@ -2,10 +2,14 @@ package org.lniranjan.stateflowexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "Mainactivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(TAG," CHECKING BRO ")
+
     }
 }
